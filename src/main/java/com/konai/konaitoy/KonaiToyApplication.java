e3ed1506a06532276e60dfeaebb825c4e9c5dd5b
양현승(Hyunseung Yang) <hs.yang@konai.com>
2022-07-25 09:20:56 +0900

    Initial commit

---
+++ src/main/java/com/konai/konaitoy/KonaiToyApplication.java
@@ -0,0 +1,13 @@
+package com.konai.konaitoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonaiToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonaiToyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konai/konaitoy/KonaiToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konai.konaitoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonaiToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
