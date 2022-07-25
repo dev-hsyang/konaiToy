5a9befd813014b82e9bc0dabac3f91fd5bb638f8
hs.yang <didgsutmd1@ajou.ac.kr>
2022-07-25 12:20:49 +0900

    prac1

--- src/main/java/com/konai/konaitoy/KonaiToyApplication.java
+++ src/main/java/com/konai/konaitoy/KonaiToyApplication.java
@@ -2,6 +2,7 @@ package com.konai.konaitoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KonaiToyApplication {
